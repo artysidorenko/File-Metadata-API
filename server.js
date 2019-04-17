d05814c4b46a446b05db366dd6a9bcf4d414b8c1
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-04-17 12:50:23 +0100

    built file meta handler

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer')
+var upload = multer()
 
 var app = express();
 
@@ -16,6 +18,20 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+
+// MAIN TASK HERE:
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const file = req.file
+  console.log(file.originalname)
+  console.log(file.mimetype)
+  console.log(file.size)
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
 });
 
 app.listen(process.env.PORT || 3000, function () {
